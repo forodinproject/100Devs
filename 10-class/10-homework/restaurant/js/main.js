74ad5e4ede8cf6844c906193077a6c86e916f5ad
forodinproject <112388027+forodinproject@users.noreply.github.com>
2024-01-29 23:13:01 +0530

    Fix Navigation

--- 10-class/10-homework/restaurant/js/main.js
+++ 10-class/10-homework/restaurant/js/main.js
@@ -1,7 +1,13 @@
-const navToggle = document.querySelector('.mobile-nav-toggle');
 
+const primaryHeader = document.querySelector('.primary-header');
+const navToggle = document.querySelector('.mobile-nav-toggle');
 const primaryNav = document.querySelector('.primary-nav');
 
 navToggle.addEventListener('click', () => {
+    primaryNav.hasAttribute('data-visible')
+        ? navToggle.setAttribute('aria-expanded', false)
+        : navToggle.setAttribute('aria-expanded', true);
     primaryNav.toggleAttribute('data-visible');
-})
+    primaryHeader.toggleAttribute('data-overlay');
+
+});
