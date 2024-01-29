bc2be6bf9310aa14d4aa7f91c606223b4e13da3b
forodinproject <112388027+forodinproject@users.noreply.github.com>
2024-01-29 21:34:02 +0530

    Make a funtional Hamburger menu

--- 10-class/10-homework/restaurant/js/main.js
+++ 10-class/10-homework/restaurant/js/main.js
@@ -1,1 +1,7 @@
+const navToggle = document.querySelector('.mobile-nav-toggle');
 
+const primaryNav = document.querySelector('.primary-nav');
+
+navToggle.addEventListener('click', () => {
+    primaryNav.toggleAttribute('data-visible');
+})
