2958c089582ffc76b3511ef697f23ef8902ec08d
forodinproject <112388027+forodinproject@users.noreply.github.com>
2024-02-26 19:12:31 +0530

    Check if day is weekend, boring or class

--- 12-class/class-weekend-boring/js/main.js
+++ 12-class/class-weekend-boring/js/main.js
@@ -4,7 +4,15 @@ function check() {
 
   const day = document.querySelector('#day').value
 
-  //Conditionals go here
+  const today = day.toLowerCase();
 
-
+  if (today === "sunday" || today === "saturday") {
+    document.querySelector("#placeToSee").innerText = "Weekend";
+  }
+  else if (today === "tuesday" || today === "thursday") {
+    document.querySelector("#placeToSee").innerText = "Class";
+  }
+  else if (today === "monday" || today === "wednesday" || today === "friday") {
+    document.querySelector("#placeToSee").innerText = "Boring";
+  }
 }
